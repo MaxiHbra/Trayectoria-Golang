0e6501894a9a0fe1def2d092a996bfd40c6ac053
MaxiHbra <maxihuebra2003@gmail.com>
2026-08-26 21:14:14 -0300

    Mi primer hola mundo

---
+++ Practica/hola_mundo.go
@@ -0,0 +1,7 @@
+package main  //Indicacion de que estamos en el paquete principal de todo el programa, este es un punto de entrada para la ejecución
+
+import "fmt" //Importando un paquete importante para mostrar datos por consola
+
+func main(){
+	fmt.Println("Hola mundo este es mi primera interacción con este lenguaje, estoy feliz!")
+}
