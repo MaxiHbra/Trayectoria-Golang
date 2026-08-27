f4c5466795ba3a5a8dab9bf8c7b03959cb20b50d
MaxiHbra <maxihuebra2003@gmail.com>
2026-08-27 10:08:09 -0300

    Tipos

---
+++ Practica/tipos_datos.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main(){
+	var edad int;
+	var nombre string;
+	var valor bool;
+	var ciudad string = "Salta"
+	var temperatura float32 23.5
+	var temperatura2 float64 25.67
+	//Pero gracias a la inferencia de tipos puedo escribir una variable sin incluir su tipo ya que la estoy asignando
+	pais := "Argentina"
+	
+}
